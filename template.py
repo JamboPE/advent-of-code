7b189d4cbe690922bdeca13a4609e10aa180b1b0
JamboPE <github@jdpe.co.uk>
2023-12-03 23:45:37 +0000

    Changed default file input

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 from AOC_utils import *
 #
-input_file = open("input","r")
+input_file = open("dev_inputA","r")
 
 
 copy_to_clipboard()
