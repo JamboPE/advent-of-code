b08321936de755b0655bf64fc3fe79c8581d9c25
JamboPE <github@jdpe.co.uk>
2024-03-06 16:48:51 +0000

    Added Days

---
+++ Day 5/AOC_Day5A.py
@@ -0,0 +1,11 @@
+# Advent of Code 2023 Day 5 Part A
+# Import packages
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from AOC_utils import *
+#
+input_file = open("Day 5/dev_inputA","r")
+
+
+#copy_to_clipboard()
